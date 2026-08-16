accc8565c76f73d10cc742253315e9fabb0b46df
Jaspreet Dhami <jdhami1@ualberta.ca>
2026-08-15 23:02:50 -0600

    added graphs for analysis by updated testing file.

--- src/my_testing_space.py
+++ src/my_testing_space.py
@@ -2,17 +2,36 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from matplotlib.collections import LineCollection
+from matplotlib.ticker import AutoMinorLocator
 
 def fetch_data(file):
     df = pd.read_csv(file)
     return df
     
-def make_tele_plot(data):
-    data.plot(x="session_time", y="speed_kph")
+def make_tele_plot(data, lap_num):
 
-    data.plot(x="session_time", y="throttle")
-   
-    data.plot(x="session_time", y="brake")
+    lap_spec_data = data[data["current_lap_num"] == lap_num]
+
+    fig, axs = plt.subplots(3,1,sharex=True)
+
+    axs[0].plot(lap_spec_data["session_time"], lap_spec_data["speed_kph"])
+    axs[0].set_title("speed")
+
+    axs[1].plot(lap_spec_data["session_time"],lap_spec_data["throttle"])
+    axs[1].set_title("throttle")
+
+    axs[2].plot(lap_spec_data["session_time"],lap_spec_data["brake"])
+    axs[2].set_title("braking")
+
+     # data.plot(x="session_time", y="speed_kph")
+    # data.plot(x="session_time", y="throttle")
+    # data.plot(x="session_time", y="brake")
+    for ax in axs:
+        ax.grid(axis="x", which="major", alpha=0.5)
+        ax.grid(axis="x", which="minor", alpha=0.2)
+        ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+
+    plt.tight_layout()
     plt.show()
 
 def make_raceline(data):
@@ -104,19 +123,53 @@ def trial_line_collection(data):
 
     plt.show()
 
-def make_heatmap(data):
+def analyze(data):
+    n = len(data)
+    b_one = 0 
+    b_zero = 0
+    sum = 0
+
+    for pt in data:
+        if pt == 0:
+            b_zero += 1
+        elif pt == 1:
+            b_one += 1
+        else:
+            sum += pt
+
+    b_non_binary = n-b_one-b_zero
+
+    print(f" 1 {(b_one/n)*100}%")
+    print(f" 0 {(b_zero/n)*100}%")
+    print(f" 0 to 1 {((b_non_binary)/n)*100}%")
+    print(f"Average of 0-1 values: {sum/b_non_binary}")
+
+    plt.hist(data, bins = 15, edgecolor = "black")
+    plt.show()
+
+    return
+
+
+def make_heatmap(data, lap_num):
 
     # fetching points and segment speeds
     points = list()
     segment_speeds = list()
     segment_throttle = list()
     segment_brake = list()
+
+    bloop = False
+    
     for index,row in data.iterrows():
-        if row["current_lap_num"] == 4:
+        if row["current_lap_num"] == lap_num:
             points.append((row["world_pos_X"], row["world_pos_Z"]))
             segment_speeds.append(row["speed_kph"])
             segment_throttle.append(row["throttle"])
             segment_brake.append(row["brake"])
+
+    if bloop:
+            analyze(segment_brake)
+            return
 
     # line collections
     segments = list()
@@ -180,6 +233,9 @@ def main():
     combined_file = "./data/processed/combined_data.csv"
     combined_df = fetch_data(combined_file)
 
-    make_heatmap(combined_df)
+    lap_number = 3
+
+    make_tele_plot(combined_df,lap_number)
+    # make_heatmap(combined_df, lap_number)
     
 main()
